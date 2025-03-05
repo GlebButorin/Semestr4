8f684d459d5f03e0f66bfcdeb0657b6557e98910
GlebButorin <glebbutorin2604@mail.ru>
2025-03-05 21:57:56 +0300

    first commit

---
+++ lab2/build/CMakeFiles/godzilla.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/godzilla.dir/godzilla.cpp.o"
+  "CMakeFiles/godzilla.dir/godzilla.cpp.o.d"
+  "godzilla"
+  "godzilla.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/godzilla.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab2/build/CMakeFiles/godzilla.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for godzilla.
+# This may be replaced when dependencies are built.
